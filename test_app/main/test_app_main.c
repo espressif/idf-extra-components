06bd5dcbe325672a40b4c8ca33b3848aa13ae2c1
Tomas Rezucha <tomas.rezucha@espressif.com>
2022-02-28 17:02:41 +0100

    ci: Publish test results

--- test_app/main/test_app_main.c
+++ test_app/main/test_app_main.c
@@ -4,15 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
-#include <string.h>
 #include "unity.h"
 
 void app_main(void)
 {
     UNITY_BEGIN();
-    //unity_run_tests_by_tag("[libsodium]", false);
     unity_run_all_tests();
-    //unity_run_menu();
     UNITY_END();
 }
