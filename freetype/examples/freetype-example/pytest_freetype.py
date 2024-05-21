c3a88bb081a381c4e35ebfb590d553e05fe91cc3
Ivan Grokhotkov <ivan@espressif.com>
2024-05-21 23:12:15 +0200

    feat(freetype): add example project

---
+++ freetype/examples/freetype-example/pytest_freetype.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Unlicense OR CC0-1.0
+import pytest
+from pytest_embedded import Dut
+
+
+@pytest.mark.supported_targets
+@pytest.mark.generic
+def test_freetype_example(dut: Dut) -> None:
+    dut.expect_exact('FreeType library initialized')
+    dut.expect_exact('Font loaded')
+    for c in 'FreeType':
+        dut.expect_exact(f'Rendering char: \'{c}\'')
