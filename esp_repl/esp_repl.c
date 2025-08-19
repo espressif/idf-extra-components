8f9b6fa7569f0265cf5613df644e8907f450ee35
Guillaume Souchere <guillaume.souchere@espressif.com>
2025-08-19 09:22:28 +0200

    feat(esp_repl): Add component to idf-extra-component

---
+++ esp_repl/esp_repl.c
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <stdbool.h>
+#include "esp_repl.h"
+#include "esp_err.h"
+
