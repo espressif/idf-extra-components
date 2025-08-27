1fe1ae0330e2b6c6a62a565ef4aa67cf3f7c3845
Marius Vikhammer <marius.vikhammer@espressif.com>
2025-08-27 12:42:27 +0800

    ci(spi_nand_flash): disable linux test for 5.1

--- spi_nand_flash/host_test/pytest_nand_flash_linux.py
+++ spi_nand_flash/host_test/pytest_nand_flash_linux.py
@@ -1,11 +1,12 @@
-# SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD                                                                                   
+# SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Unlicense OR CC0-1.0
 import pytest
-from pytest_embedded import Dut 
+from pytest_embedded import Dut
 from pytest_embedded_idf.utils import idf_parametrize
 
 
 @pytest.mark.host_test
 @idf_parametrize('target', ['linux'], indirect=['target'])
+@pytest.mark.skip_if_soc("IDF_VERSION_MAJOR <= 5 and IDF_VERSION_MINOR < 3") # Linux apps not built for 5.1~5.2
 def test_nand_flash_linux(dut: Dut) -> None:
     dut.expect_exact('All tests passed', timeout=120)
