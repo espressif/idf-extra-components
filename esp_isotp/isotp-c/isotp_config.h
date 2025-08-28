465046b74311e0fe6ae38a98214d140563edec8d
eternal-echo <yu.yuan@sjtu.edu.cn>
2025-08-28 18:47:32 +0800

    feat: Add isotp-c library
    Imported the isotp-c library code from a specific commit of 083709f57d8fc964e7ac5dd0c7ac7aef6364f156.
    https://github.com/lishen2/isotp-c
    https://github.com/SimonCahill/isotp-c

---
+++ esp_isotp/isotp-c/isotp_config.h
@@ -0,0 +1,58 @@
+////////////////////////////////////////////////////////////////////////
+//                  ___ ___  ___ _____ ___      ___                   //
+//                 |_ _/ __|/ _ \_   _| _ \___ / __|                  //
+//                  | |\__ \ (_) || | |  _/___| (__                   //
+//                 |___|___/\___/ |_| |_|      \___|                  //
+//                                                                    //
+//                      ___ ___  _  _ ___ ___ ___                     //
+//                     / __/ _ \| \| | __|_ _/ __|                    //
+//                    | (_| (_) | .` | _| | | (_ |                    //
+//                     \___\___/|_|\_|_| |___\___|                    //
+//                                                                    //
+////////////////////////////////////////////////////////////////////////
+
+#ifndef ISOTPC_CONFIG_H
+#define ISOTPC_CONFIG_H
+
+/* Max number of messages the receiver can receive at one time, this value
+ * is affected by can driver queue length
+ */
+#define ISO_TP_DEFAULT_BLOCK_SIZE 8
+
+/* The STmin parameter value specifies the minimum time gap allowed between
+ * the transmission of consecutive frame network protocol data units
+ */
+#define ISO_TP_DEFAULT_ST_MIN_US 1000
+
+/* This parameter indicate how many FC N_PDU WTs can be transmitted by the
+ * receiver in a row.
+ */
+#define ISO_TP_MAX_WFT_NUMBER 1
+
+/* Private: The default timeout to use when waiting for a response during a
+ * multi-frame send or receive.
+ */
+#define ISO_TP_DEFAULT_RESPONSE_TIMEOUT_US 100000
+
+/* Private: Determines if by default, padding is added to ISO-TP message frames.
+ */
+// #define ISO_TP_FRAME_PADDING
+
+/* Private: Value to use when padding frames if enabled by ISO_TP_FRAME_PADDING
+ */
+#ifndef ISO_TP_FRAME_PADDING_VALUE
+#define ISO_TP_FRAME_PADDING_VALUE 0xAA
+#endif
+
+/* Private: Determines if by default, an additional argument is present in the
+ * definition of isotp_user_send_can.
+ */
+#define ISO_TP_USER_SEND_CAN_ARG
+
+/* Enable support for transmission complete callback */
+#define ISO_TP_TRANSMIT_COMPLETE_CALLBACK
+
+/* Enable support for receive complete callback */
+#define ISO_TP_RECEIVE_COMPLETE_CALLBACK
+
+#endif // ISOTPC_CONFIG_H
