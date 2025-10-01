e6fbaaa8007250ad24b6e2c5838890f72b926f5b
Guillaume Souchere <guillaume.souchere@espressif.com>
2025-10-01 12:54:48 +0200

    feat(esp_commands): Add a way to output data  on specified file descriptor

--- esp_commands/include/esp_commands.h
+++ esp_commands/include/esp_commands.h
@@ -18,11 +18,12 @@ extern "C" {
  * This function type is used to implement a console command.
  *
  * @param context User-defined context passed at invocation
+ * @param fd_out The file descriptor to use to output data
  * @param argc Number of arguments
  * @param argv Array of argc entries, each pointing to a null-terminated string argument
  * @return Return code of the console command; 0 indicates success
  */
-typedef int (*esp_command_func_t)(void *context, int argc, char **argv);
+typedef int (*esp_command_func_t)(void *context, const int fd_out, int argc, char **argv);
 
 /**
  * @brief Callback to generate a command hint
@@ -118,13 +119,24 @@ DEFINE_FIELD_ACCESSOR(help)
 #define FIELD_ACCESSOR(NAME) get_##NAME
 
 /**
+ * @brief Function pointer type for writing bytes.
+ *
+ * @param fd File descriptor.
+ * @param buf Buffer containing bytes to write.
+ * @param count Number of bytes to write.
+ * @return Number of bytes written, or -1 on error.
+ */
+typedef ssize_t (*esp_commands_write_t)(int fd, const void *buf, size_t count);
+
+/**
  * @brief Configuration parameters for esp_commands_manager initialization
  */
 typedef struct esp_commands_config {
-    size_t max_cmdline_length;  /*!< Maximum length of the command line buffer, in bytes */
-    size_t max_cmdline_args;    /*!< Maximum number of command line arguments to parse */
-    int hint_color;             /*!< ANSI color code used for hint text */
-    bool hint_bold;             /*!< If true, display hint text in bold */
+    esp_commands_write_t write_func;    /*!< Write function to call when executing a command */
+    size_t max_cmdline_length;          /*!< Maximum length of the command line buffer, in bytes */
+    size_t max_cmdline_args;            /*!< Maximum number of command line arguments to parse */
+    int hint_color;                     /*!< ANSI color code used for hint text */
+    bool hint_bold;                     /*!< If true, display hint text in bold */
 } esp_commands_config_t;
 
 /**
@@ -143,9 +155,10 @@ typedef struct esp_commands_config {
  *
  * This callback is called when a command is successfully completed.
  *
+ * @param cb_ctx Opaque pointer pointing at the context passed to the callback
  * @param completed_cmd_name Completed command name
  */
-typedef void (*esp_command_get_completion_t)(const char *completed_cmd_name);
+typedef void (*esp_command_get_completion_t)(void *cb_ctx, const char *completed_cmd_name);
 
 /**
  * @brief Callback to retrieve a string field of esp_command_t
@@ -207,14 +220,15 @@ esp_err_t esp_commands_unregister_cmd(const char *cmd_name);
  * @brief Execute a command line
  *
  * @param cmd_set Set of commands allowed to execute. If NULL, all registered commands are allowed
+ * @param cmd_fd File descriptor used to output data
  * @param cmd_line Command line string to execute
- * @param cmd_ret Return value from the command function
+ * @param cmd_ret Return value from the command function. If -1, standard output will be used.
  * @return ESP_OK on success
  *         ESP_ERR_INVALID_ARG if the command line is empty or only whitespace
  *         ESP_ERR_NOT_FOUND if command is not found in cmd_set
  *         ESP_ERR_NO_MEM if internal memory allocation fails
  */
-esp_err_t esp_commands_execute(esp_command_set_handle_t cmd_set, const char *cmdline, int *cmd_ret);
+esp_err_t esp_commands_execute(esp_command_set_handle_t cmd_set, const int cmd_fd, const char *cmdline, int *cmd_ret);
 
 /**
  * @brief Find a command by name within a specific command set.
@@ -235,9 +249,10 @@ esp_command_t *esp_commands_find_command(esp_command_set_handle_t cmd_set, const
  *
  * @param cmd_set Set of commands allowed for completion. If NULL, all registered commands are used
  * @param buf Input string typed by the user
+ * @param cb_ctx context passed to the completion callback
  * @param completion_cb Callback to return completed command names
  */
-void esp_commands_get_completion(esp_command_set_handle_t cmd_set, const char *buf, esp_command_get_completion_t completion_cb);
+void esp_commands_get_completion(esp_command_set_handle_t cmd_set, const char *buf, void *cb_ctx, esp_command_get_completion_t completion_cb);
 
 /**
  * @brief Provide command hint for linenoise library
@@ -297,6 +312,17 @@ esp_command_set_handle_t esp_commands_concat_cmd_set(esp_command_set_handle_t cm
  * @param cmd_set Pointer to the handle of the command set to destroy
  */
 void esp_commands_destroy_cmd_set(esp_command_set_handle_t *cmd_set);
+
+/**
+ * @brief Split a command line and populate argc and argv parameters
+ *
+ * @param line the line that has to be split into arguments
+ * @param argv array of arguments created from the line
+ * @param argv_size size of the argument array
+ * @return size_t number of arguments found in the line and stored
+ * in argv
+ */
+size_t esp_commands_split_argv(char *line, char **argv, size_t argv_size);
 
 #ifdef __cplusplus
 }
