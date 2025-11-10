f530ab251ae5d1a36a9efa0becf18b58538c5af8
sonika.rathi <sonika.rathi@espressif.com>
2025-11-10 13:13:54 +0100

    feat(spi_nand_flash): update test_app, host_tests and documentation for block device support

--- spi_nand_flash/examples/nand_flash/pytest_nand_flash_example.py
+++ spi_nand_flash/examples/nand_flash/pytest_nand_flash_example.py
@@ -1,7 +1,18 @@
+# SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Unlicense OR CC0-1.0
 import pytest
+from pytest_embedded import Dut
+from pytest_embedded_idf.utils import idf_parametrize
+import glob
+from pathlib import Path
 
 
 @pytest.mark.spi_nand_flash
+@pytest.mark.skipif(
+    not bool(glob.glob(f'{Path(__file__).parent.absolute()}/build*/')),
+    reason="Skip the idf version that not build"
+)
+@idf_parametrize('target', ['esp32'], indirect=['target'])
 def test_nand_flash_example(dut) -> None:
     dut.expect_exact("Opening file")
     dut.expect_exact("File written")
