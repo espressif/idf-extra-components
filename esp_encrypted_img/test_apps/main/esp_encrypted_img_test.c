c0638e5fbdf9861c0f51b6ab09fb246411b773be
Mahavir Jain <mahavir@espressif.com>
2025-12-31 14:10:22 +0530

    feat(esp_encrypted_img): add support for PSA crypto interface

--- esp_encrypted_img/test_apps/main/esp_encrypted_img_test.c
+++ esp_encrypted_img/test_apps/main/esp_encrypted_img_test.c
@@ -12,21 +12,41 @@
 #include "unity_test_utils_memory.h"
 
 #include "soc/soc_caps.h"
+
+#if defined(CONFIG_MBEDTLS_VER_4_X_SUPPORT)
+#include "psa/crypto.h"
+#else
 #include "mbedtls/aes.h"
+#endif
 
 void setUp(void)
 {
 #if SOC_AES_SUPPORTED
-    // Execute mbedtls_aes_init operation to allocate AES interrupt
+    // Execute AES operation to allocate AES interrupt
     // allocation memory which is considered as leak otherwise
     const uint8_t plaintext[16] = {0};
     uint8_t ciphertext[16];
     const uint8_t key[16] = { 0 };
+#if defined(CONFIG_MBEDTLS_VER_4_X_SUPPORT)
+    psa_crypto_init();
+    psa_key_attributes_t attributes = PSA_KEY_ATTRIBUTES_INIT;
+    psa_set_key_usage_flags(&attributes, PSA_KEY_USAGE_ENCRYPT);
+    psa_set_key_algorithm(&attributes, PSA_ALG_ECB_NO_PADDING);
+    psa_set_key_type(&attributes, PSA_KEY_TYPE_AES);
+    psa_set_key_bits(&attributes, 128);
+    psa_key_id_t key_id;
+    psa_import_key(&attributes, key, sizeof(key), &key_id);
+    size_t output_length;
+    psa_cipher_encrypt(key_id, PSA_ALG_ECB_NO_PADDING, plaintext, sizeof(plaintext),
+                       ciphertext, sizeof(ciphertext), &output_length);
+    psa_destroy_key(key_id);
+#else
     mbedtls_aes_context ctx;
     mbedtls_aes_init(&ctx);
     mbedtls_aes_setkey_enc(&ctx, key, 128);
     mbedtls_aes_crypt_ecb(&ctx, MBEDTLS_AES_ENCRYPT, plaintext, ciphertext);
     mbedtls_aes_free(&ctx);
+#endif
 #endif // SOC_AES_SUPPORTED
     unity_utils_record_free_mem();
 }
