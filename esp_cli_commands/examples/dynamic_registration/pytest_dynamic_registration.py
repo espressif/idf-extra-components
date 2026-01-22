97e1baefd62a685115124eb481e6ba868789c743
Guillaume Souchere <guillaume.souchere@espressif.com>
2026-01-22 12:41:49 +0100

    fix(esp_cli_commands): Update pytests in examples to fix CI

--- esp_cli_commands/examples/dynamic_registration/pytest_dynamic_registration.py
+++ esp_cli_commands/examples/dynamic_registration/pytest_dynamic_registration.py
@@ -1,11 +1,17 @@
 # SPDX-FileCopyrightText: 2023-2025 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Unlicense OR CC0-1.0
 import pytest
-from pytest_embedded_idf.dut import IdfDut
+from pytest_embedded import Dut
 from pytest_embedded_idf.utils import idf_parametrize
+import glob
+from pathlib import Path
 
 
 @pytest.mark.host_test
+@pytest.mark.skipif(
+    not bool(glob.glob(f'{Path(__file__).parent.absolute()}/build*/')),
+    reason="Skip the idf version that not build"
+)
 @idf_parametrize('target', ['linux'], indirect=['target'])
-def test_dynamic_registration(dut: IdfDut) -> None:
+def test_dynamic_registration(dut: Dut) -> None:
     dut.expect("end of example", timeout=10)
