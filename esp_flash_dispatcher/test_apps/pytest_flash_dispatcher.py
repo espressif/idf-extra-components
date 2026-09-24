d9f2e3d055f540df6b30bd3fe22e8bb871b00ea8
igor.udot <igor.udot@espressif.com>
2026-09-24 14:05:43 +0800

    ci: added targets for all pytest files

--- esp_flash_dispatcher/test_apps/pytest_flash_dispatcher.py
+++ esp_flash_dispatcher/test_apps/pytest_flash_dispatcher.py
@@ -11,7 +11,7 @@ def test_esp_flash_dispatcher(dut) -> None:
 
 
 @pytest.mark.generic
-@idf_parametrize('target', ['esp32', 'esp32h4', 'esp32p4', 'esp32s31', 'esp32c61'], indirect=['target'])
+@idf_parametrize('target', ['esp32', 'esp32h4', 'esp32p4', 'esp32s3', 'esp32c61'], indirect=['target'])
 def test_esp_flash_dispatcher_generic(dut) -> None:
     dut.run_all_single_board_cases()
 
